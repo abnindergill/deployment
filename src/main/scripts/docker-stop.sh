9be250810663aa083e84c24bd0a2f4875682a882
abninderg <abninder@btinternet.com>
2020-03-25 21:29:30 +0000

    no message

--- src/main/scripts/docker-stop.sh
+++ src/main/scripts/docker-stop.sh
@@ -2,6 +2,7 @@
 
 containerId=$(docker ps | grep 'abninder/test-image' | awk '{ print $1 }')
 
+echo "checking if container is running ...."
 if [ -n "${containerId}" ]; then
     echo "stopping container with id :" $containerId
     docker kill $containerId
