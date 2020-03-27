fb3fa7a5a2c721ae27ac33aa90d525d7f6b4c8bb
abninderg <abninder@btinternet.com>
2020-03-27 20:32:53 +0000

    "modified scripts"

---
+++ target/api/docker-stop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+imageName="$1"
+
+echo "image name is ${imageName}"
+
+containerId=$(docker ps | grep "${imageName}" | awk '{ print $1 }')
+echo "checking if container is running ...." >> /tmp/testfile
+echo "container id ${containerId} found" >> /tmp/testfile
+
+if [ -n "${containerId}" ]; then
+    echo "stopping container with id : $containerId" >> /tmp/testfile
+    echo "======================================================"
+    docker kill "$containerId"
+fi
+
+
