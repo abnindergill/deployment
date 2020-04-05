b4d08208878d47f02220554b60f2e5629accaf26
abninderg <abninder@btinternet.com>
2020-04-05 21:33:25 +0100

    "create script to spin up new ec2 instance"

--- src/main/scripts/prepareEC2Instance.sh
+++ src/main/scripts/prepareEC2Instance.sh
@@ -13,7 +13,7 @@ instanceId=$(./checkRunningEc2Instances.sh aws)
 if [[ $instanceId ]]; then
    echo instance id is: ${instanceId}
    publicDns=$(${aws} ec2 describe-instances --instance-ids ${instanceId} --query 'Reservations[].Instances[].PublicDnsName' --output text)
-   echo public dns is: ${publicDns}
+   echo ${publicDns}
 else
    securityGroupId=$(./configureSecurityGroup.sh ${aws} ${SECURITY_GROUP_NAME})
    echo security group id is: ${securityGroupId}
@@ -24,7 +24,7 @@ else
    ./authoriseSecurityGroups.sh ${aws} ${securityGroupId} ${region}
 
    ./createPemKey.sh ${aws} ${EC2_PEM_KEY_PATH} ${PEM_KEY_NAME}
-   dns=$(./createNewEc2Instance.sh ${aws} ${PEM_KEY_NAME} ${SECURITY_GROUP_NAME} ${region})
-   echo ${dns}
+   publicDns=$(./createNewEc2Instance.sh ${aws} ${PEM_KEY_NAME} ${SECURITY_GROUP_NAME} ${region})
+   echo ${publicDns}
 fi
 
