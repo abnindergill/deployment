e5349cfc428abdb818d8e7ba747a5f1fd79c1653
abninderg <abninder@btinternet.com>
2020-04-06 10:16:42 +0100

    "create script to spin up new ec2 instance"

--- src/main/scripts/authoriseSecurityGroups.sh
+++ src/main/scripts/authoriseSecurityGroups.sh
@@ -5,8 +5,8 @@ securityGroupId="$2"
 region="$3"
 
 ${aws} ec2 authorize-security-group-ingress --group-id ${securityGroupId} --protocol tcp --port 22  --cidr 0.0.0.0/0 --region ${region}
-echo "enabled ssh for security group id: ${securityGroupId} on port 22"
+echo enabled ssh for security group id: ${securityGroupId} on port 22
 
 ${aws} ec2 authorize-security-group-ingress --group-id ${securityGroupId} --protocol all --port -1  --cidr 0.0.0.0/0 --region ${region}
-echo "enabled all traffic for security group id: ${securityGroupId}"
+echo enabled all traffic for security group id: ${securityGroupId}
 
--- src/main/scripts/createPemKey.sh
+++ src/main/scripts/createPemKey.sh
@@ -7,5 +7,6 @@ pem_key_name="$3"
 keyPairName=$(${aws} ec2 describe-key-pairs --key-name ${pem_key_name})
 if [ -z "${keyPairName}" ]; then
     ${aws} ec2 create-key-pair --key-name ${pem_key_name} --query 'KeyMaterial' --output text > ${pem_key_path}
+    echo created pem key for key name: ${pem_key_name} in path: ${pem_key_path}
     chmod 400 ${pem_key_path}
 fi
