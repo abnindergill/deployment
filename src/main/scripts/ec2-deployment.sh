efeed4725706008c85995ae04d2de843087d954f
abninderg <abninder@btinternet.com>
2020-04-06 15:36:35 +0100

    "create script to spin up new ec2 instance"

--- src/main/scripts/ec2-deployment.sh
+++ src/main/scripts/ec2-deployment.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-echo workspace is ${WORKSPACE}
-echo image name is ${IMAGE_NAME}
-echo last successful build is ${LAST_SUCCESSFUL_BUILD}
-echo build number is ${BUILD_NUMBER}
-echo public dns is ${PUBLIC_DNS}
-echo pem key path is ${EC2_PEM_KEY_PATH}
-
 SCRIPTS_SRC_PATH="${WORKSPACE}/docker*.sh"
 SCRIPTS_DESTINATION_FOLDER="/home/ec2-user/scripts"
 
 DOCKER_CONTAINER_STARTUP_CMD="sudo docker run -p 8082:8085 -e LISTEN_PORT=8085 ${IMAGE_NAME}:${BUILD_NUMBER}"
 
-ssh-keyscan -T 240 -H ${PUBLIC_DNS} >> ~/.ssh/known_hosts
+ssh-keygen -R $PUBLIC_DNS
 
 echo creating directory ${SCRIPTS_DESTINATION_FOLDER} on ${EC2_PUBLIC_DNS} if it doesnt exist
 ssh -i ${EC2_PEM_KEY_PATH} "ec2-user@${EC2_PUBLIC_DNS}" mkdir -p ${SCRIPTS_DESTINATION_FOLDER}
